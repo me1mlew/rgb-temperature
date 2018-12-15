74d467ddc99ae6a20079e8aaaf6161da23d4d3d0
Michael Lewis <me1mlew@ami.shef.ac.uk>
2018-12-15 10:56:16 +0000

    Testing requirements.txt

--- fabfile.py
+++ fabfile.py
@@ -3,10 +3,12 @@ from __future__ import with_statement
 from fabric import *
 import json
 
+#setup
+
 with open('project-info.json',encoding = 'UTF-8') as f:
         config = json.loads(f.read(),encoding='utf-8')
 
-project_dir = config['projectDir'] + config['project']
+project_dir = config['projectDir']
 project = config['project']
 
 c = Connection(config["host"],config["user"])
@@ -15,6 +17,14 @@ c.config.run['replace_env'] = False
 detailedTag = c.local('git describe --tags',hide=True)
 tag = float(detailedTag.stdout[1:4].strip())
 
+@task
+def commit(arg):
+	c.local('pipreqs --force .')
+	c.local('git add -p')
+	c.local('git commit')
+	c.local('git push')
+
+#end setup
 @task
 def release(arg):
     iterateTag(0.1)
@@ -31,28 +41,27 @@ def iterateTag(step):
     print("Upgrading to version: v" + str(tag))
 
 def createNewTag():
-    #todo create new tag in github
     global tag
     c.local('git tag ' + tag)
     c.local('git push origin' + tag)
 
 def checkRemoteMachine():
-    #todo check if dir is on remote machine
-    global tag 
-    test='/usr/local/bin/test'
-    exists = c.run("[ -d {} ] && echo 'True' || echo 'False'".format(project_dir)).stdout.strip()
+    global tag
+    exists = c.run("[ -d {}{} ] && echo 'True' || echo 'False'".format(project_dir,project)).stdout.strip()
 
     if(exists == 'False'):
         c.sudo("mkdir {}".format(project_dir))
-    
+        with c.cd(project_dir):
+                c.run("git clone {}".format(config['repo']))
+            
 def checkoutTag():
-    #todo checkout tag on remote machine
-    global tag
+    with c.cd("{}{}".format(project_dir,project)):
+        c.run("git checkout tags/v{}".format(str(tag)))
+
 
 def buildDependancies():
-    #todo build dependancies
-    global tag
+	with c.cd("{}{}".format(project_dir,project)):
+		c.run("pip install -r requirements.txt")
 
 def restartService():
-    #retart the applciation on the machine
-    global tag
+    c.run("{}".format(config['entry']))
