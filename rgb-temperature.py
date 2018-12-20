f4f841cea2fe69365b5dc9edcef0e303d7f588e3
Michael Lewis <me1mlew@ami.shef.ac.uk>
2018-12-20 20:26:08 +0000

    added functionality to check bulb locations against current bulb and instantiate.

--- rgb-temperature.py
+++ rgb-temperature.py
@@ -1,14 +1,30 @@
 import paho.mqtt.client as mqtt
 import time
+import yeelight as yee
+import json
 
 #app parameters
 SAMPLE_RATE = 30
 TOPIC = "sensors/temp"
+BULB_LOCATION="FRONT_BEDROOM"
 
 #MQTT setup and subscription
-client = mqtt.Client('rgb-bulb-controller')
+client = mqtt.Client('rgb-bulb-controll er')
 client.connect("raspberrypi")
 client.subscribe("sensors/temp")
+
+with open('bulb-locations.json',encoding = 'UTF-8') as f:
+    bulb_locations = json.loads(f.read(),encoding='utf-8')
+
+for bulb_location in bulb_locations:
+    if bulb_location['location'] == BULB_LOCATION:
+        bulb_id = bulb_location['id']
+
+#bulb discovery
+def getBulb():
+    for bulb_meta in yee.discover_bulbs():
+        if bulb_meta['capabilities']['id'] == bulb_id:
+           return yee.Bulb(bulb_meta['ip'])
 
 def on_message(client, userdata, message):
     print("message received " ,str(message.payload.decode("utf-8")))
@@ -18,6 +34,10 @@ def on_message(client, userdata, message):
 
 client.on_message=on_message
 client.loop_start()
+
+bulb = getBulb()
+
+bulb.set_rgb(50,64,10)
 #end
 
 while True:
